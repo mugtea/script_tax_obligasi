b497eb59539c60ebb64d6492b88909b6d332e2a3
Mukti Wibowo <Mukti.Wibowo@sahassa.co.id>
2024-02-08 17:37:38 +0700

    add services object

---
+++ Services/EmailAgentServiceTax/InstallEmailAgentsServices.bat
@@ -0,0 +1,5 @@
+
+
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil" "D:\Project\EStatement\Source Code\Core\EmailAgentService Enhancement\EmailAgentService\bin\Release\EmailAgentService.exe"
+
+pause
---
+++ Services/EmailAgentServiceTax/UninstallEmailAgentsServices.bat
@@ -0,0 +1,4 @@
+
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil" "D:\Project\EStatement\Source Code\Core\EmailAgentService Enhancement\EmailAgentService\bin\Release\EmailAgentService.exe" /u
+
+pause
